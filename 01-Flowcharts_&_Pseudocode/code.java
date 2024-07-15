4f9de582c9f895cfa1a684e37d8e5cfa73df84f7
akashdeep023 <ad3500476@gmail.com>
2024-07-15 18:15:57 +0530

    Update Flowcharts & Psedocode

---
+++ 01-Flowcharts_&_Pseudocode/code.java
@@ -0,0 +1,46 @@
+// Diagram to represent solutions of problems.
+
+// Start / Exit     -> Start or Exit
+// Input / Output   -> Read or Print 'Hi'
+// Process          -> name = "Akash" or a = 10 , b = 5
+// Decision         -> isMarks > 33 -> Yes or NO
+
+// ----------------------------------------------------------------
+// - -> (Minus)
+// + -> (Plus)
+// * -> (Multiply)
+// / -> (Divide)
+// % -> (Modulus/Remainder)
+
+// / -> (Forward slash)
+// \ -> (Back slash)
+// ----------------------------------------------------------------
+
+// Sum of 2 numbers--------------------------------
+// Flowchart            Pseudocode
+// Start                1.Start
+// Input a & b          2.Input number a & b
+// Sum a & b            3.Calculate sum = a + b
+// Print sum            4.Print sum
+// Exit                 5.Exit
+
+// Calculate Simple Interest--------------------------------
+// Flowchart            Pseudocode
+// Start                1.Start
+// Input P,R,T          2.Input principal (P),Rate (R) & Time (T)
+// SI = (P*R*T)/100     3.Calculate SI = (P*R*T)/100
+// Print SI             4.Print SI
+// Exit                 5.Exit
+
+// Find if a number is prime--------------------------------
+// Flowchart                    Pseudocode
+// Start                        1.Start
+// Input n                      2.Input n
+// let div = 2                  3.let div = 2  
+// is div < n                   4.while div < n do
+// NO -> Print Prime                if n % div == 0 do -> Print Not Prime -> Exit
+// YES -> is n % div == 0           else  -> div = div + 1  
+// NO -> div = div + 1          5.Print Prime         
+// YES -> Print Not Prime       6.Exit
+// Exit
+
