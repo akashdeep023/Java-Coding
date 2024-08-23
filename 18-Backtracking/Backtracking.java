8ef8bba130a06d954911b2359fe24619c0635f35
akashdeep023 <ad3500476@gmail.com>
2024-08-23 20:22:32 +0530

    Find Permutation Solve

--- 18-Backtracking/Backtracking.java
+++ 18-Backtracking/Backtracking.java
@@ -7,7 +7,7 @@ public class Backtracking {
         System.out.println(arr[arr.length - 1] + " )");
     }
 
-    public static void changeArr(int arr[], int i, int val) {
+    public static void changeArr(int arr[], int i, int val) { // TC -> O(n)
         // Base case
         if (i == arr.length) {
             printArr(arr);
@@ -17,6 +17,52 @@ public class Backtracking {
         arr[i] = val;
         changeArr(arr, i + 1, val + 1); // fnx call step
         arr[i] = arr[i] - 2; // backtracking step
+    }
+
+    public static void findSubSetString(String str, String ans, int i) { // TC -> O(n * n^2) & SC-> O(n)
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+        // Yes choice
+        findSubSetString(str, ans + str.charAt(i), i + 1);
+        // No choice
+        findSubSetString(str, ans, i + 1);
+    }
+
+    public static void findSubSetStringBuilder(String str, StringBuilder ans, int i) {
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+        // Yes choice
+        findSubSetStringBuilder(str, ans.append(str.charAt(i)), i + 1);
+        ans = ans.delete(ans.length() - 1, ans.length()); // remove last character
+        // No choice
+        findSubSetStringBuilder(str, ans, i + 1);
+    }
+
+    public static void findPermutation(String str, String ans) { // TC -> O(n * n!)
+        // Base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        // Recursion
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            // "abcde"="ab"+"de"="abde"
+            String newStr = str.substring(0, i) + str.substring(i + 1, str.length()); // str.substring(start,end) Ya str.substring(start) => str.substring(start,str.length())
+            findPermutation(newStr, ans + curr);
+        }
     }
 
     public static void main(String[] args) {
@@ -29,5 +75,20 @@ public class Backtracking {
         int arr[] = new int[5];
         changeArr(arr, 0, 1);
         printArr(arr);
+        System.out.println();
+
+        // Find Subsets --------------------------------
+        String str = "abc";
+        findSubSetString(str, "", 0);
+        System.out.println();
+
+        findSubSetStringBuilder(str, new StringBuilder(""), 0);
+        System.out.println();
+
+        // Find Permutations --------------------------------
+        String str2 = "abc";
+        findPermutation(str2, "");
+        System.out.println();
+
     }
 }
