da8fecabedf7a3e7a77018673e86feb2e6e6c6d7
akashdeep023 <ad3500476@gmail.com>
2024-08-28 04:02:38 +0530

    Complete Backtracking Class

--- 18-Backtracking/Backtracking.java
+++ 18-Backtracking/Backtracking.java
@@ -134,6 +134,81 @@ public class Backtracking {
         return false;
     }
 
+    public static int gridWays(int i, int j, int n, int m) {
+        // Base case
+        if (i == n - 1 && j == m - 1) { // condition for last cell
+            return 1; // 1 way
+        } else if (i == n || j == m) { // boundary cross condition
+            return 0; // 0 way
+        }
+        int w1 = gridWays(i + 1, j, n, m);
+        int w2 = gridWays(i, j + 1, n, m);
+        return w1 + w2;
+    }
+
+    public static boolean sudokuSolver(int sudoku[][], int row, int col) {
+        // base case
+        if (row == 9) {
+            return true;
+        }
+        //recursion
+        int nextRow = row, nextCol = col + 1;
+        if (col + 1 == 9) {
+            nextRow = row + 1;
+            nextCol = 0;
+        }
+        if (sudoku[row][col] != 0) { // already placed
+            return sudokuSolver(sudoku, nextRow, nextCol);
+        }
+        for (int digit = 1; digit <= 9; digit++) { // choice digit 1 to 9
+            if (isSafeSudoku(sudoku, row, col, digit)) {
+                sudoku[row][col] = digit;
+                if (sudokuSolver(sudoku, nextRow, nextCol)) { // solution exists
+                    return true;
+                }
+                sudoku[row][col] = 0; // backtracking
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSafeSudoku(int sudoku[][], int row, int col, int digit) {
+        //column
+        for (int i = 0; i <= 8; i++) {
+            if (sudoku[i][col] == digit) {
+                return false;
+            }
+        }
+        //row
+        for (int j = 0; j <= 8; j++) {
+            if (sudoku[row][j] == digit) {
+                return false;
+            }
+        }
+        // grid (3 x 3)
+        int sr = (row / 3) * 3; // 1,2,3
+        int sc = (col / 3) * 3; // 1,2,3
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoku[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void printSudoku(int sudoku[][]) {
+        for (int i = 0; i < sudoku.length; i++) {
+            for (int j = 0; j < sudoku.length; j++) {
+                System.out.print(sudoku[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------
+
     public static void main(String[] args) {
         // Type of Backtracking --------------------------------
         // 1. Decision
@@ -183,6 +258,32 @@ public class Backtracking {
         } else {
             System.out.println("Solution is not possible");
         }
+        System.out.println();
 
+        // Grid Ways --------------------------------
+        // Find number of ways to reach from (0,0) to (N-1,M-1) in a NxM grid. (Allowed moves - right and down) ---
+        int row = 3, col = 3;
+        System.out.println("Number of ways : " + gridWays(0, 0, row, col));
+        System.out.println();
+
+        // Sudoku --------------------------------
+        // Write a function to complete a Sudoku. ---
+        int sudoku[][] = {
+                { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
+                { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
+                { 0, 0, 3, 0, 0, 4, 1, 9, 0 },
+                { 1, 8, 5, 0, 6, 0, 0, 2, 0 },
+                { 0, 0, 0, 0, 2, 0, 0, 6, 0 },
+                { 9, 6, 0, 4, 0, 5, 3, 0, 0 },
+                { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
+                { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
+                { 8, 2, 7, 0, 0, 9, 0, 1, 3 }
+        };
+        if (sudokuSolver(sudoku, 0, 0)) {
+            System.out.println("----------Solution exists----------");
+            printSudoku(sudoku);
+        } else {
+            System.out.println("----------Solution not exists----------");
+        }
     }
 }
