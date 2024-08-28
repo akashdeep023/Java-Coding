5ff801a74c3832c8bceb9fcd51da5db280a5fa70
akashdeep023 <ad3500476@gmail.com>
2024-08-29 01:04:27 +0530

    Complete Assignment

--- 18-Backtracking/Assignment_Backtracking.java
+++ 18-Backtracking/Assignment_Backtracking.java
@@ -1,4 +1,5 @@
 public class Assignment_Backtracking {
+    // Solution 1 . --------------------------------
     public static void ratInMaze(int maze[][], int row, int col, int newMaze[][]) {
         // base case
         if (row == maze.length - 1 && col == maze.length - 1) {
@@ -27,6 +28,83 @@ public class Assignment_Backtracking {
             System.out.println();
         }
         System.out.println();
+    }
+
+    // Solution 2 . --------------------------------
+    final static char[][] L = { {}, {}, { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' },
+            { 'j', 'k', 'l' }, { 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' },
+            { 't', 'u', 'v' }, { 'w', 'x', 'y', 'z' } };
+
+    public static void letterCombinations(String D) {
+        int len = D.length();
+        if (len == 0) {
+            System.out.print("null");
+            return;
+        }
+        bfs(0, len, new StringBuilder(), D);
+    }
+
+    public static void bfs(int pos, int len, StringBuilder sb, String D) {
+        if (pos == len) {
+            System.out.print(sb.toString());
+        } else {
+            char[] letters = L[Character.getNumericValue(D.charAt(pos))];
+            for (int i = 0; i < letters.length; i++)
+                bfs(pos + 1, len, new StringBuilder(sb).append(letters[i]), D);
+        }
+    }
+
+    // Solution 3 . --------------------------------
+    static int N = 8;
+
+    public static boolean isSafe(int x, int y, int sol[][]) {
+        return (x >= 0 && x < N && y >= 0 && y < N
+                && sol[x][y] == -1);
+    }
+
+    public static void printSolution(int sol[][]) {
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < N; y++)
+                System.out.print(sol[x][y] + " ");
+            System.out.println();
+        }
+    }
+
+    public static boolean solveKT(int n) {
+        int sol[][] = new int[n][n];
+        for (int x = 0; x < N; x++)
+            for (int y = 0; y < N; y++)
+                sol[x][y] = -1;
+        int xMove[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int yMove[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        //As the Knight starts from cell(0,0)
+        sol[0][0] = 0;
+        if (!solveKTUtil(0, 0, 1, sol, xMove, yMove)) {
+            System.out.println("Solution does not exist");
+            return false;
+        } else
+            printSolution(sol);
+        return true;
+    }
+
+    public static boolean solveKTUtil(int x, int y, int movei, int sol[][],
+            int xMove[], int yMove[]) {
+        int k, next_x, next_y;
+        if (movei == N * N)
+            return true;
+        for (k = 0; k < 8; k++) {
+            next_x = x + xMove[k];
+            next_y = y + yMove[k];
+            if (isSafe(next_x, next_y, sol)) {
+                sol[next_x][next_y] = movei;
+                if (solveKTUtil(next_x, next_y, movei + 1,
+                        sol, xMove, yMove))
+                    return true;
+                else
+                    sol[next_x][next_y] = -1; // backtracking
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -60,8 +138,14 @@ public class Assignment_Backtracking {
         // A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1
         // does not map to any letters.
         String digits1 = "23"; // Output : "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"
+        letterCombinations(digits1);
+        System.out.println();
         String digits2 = "2"; // Output : "a", "b", "c"
+        letterCombinations(digits2);
+        System.out.println();
         String digits3 = ""; // Output : ""
+        letterCombinations(digits3);
+        System.out.println("\n");
 
         // Question 3 : Knight’s Tour --------------------------------
         // Given a N*N board with the Knight placed on the first block of an empty board. Moving
@@ -76,6 +160,7 @@ public class Assignment_Backtracking {
         //         47 50 45 54 25 20 11 14
         //         56 43 52  3 22 13 24  5
         //         51 46 55 44 53  4 21 12
+        solveKT(N);
 
     }
 }
