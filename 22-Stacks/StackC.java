283ee03e4b217c2eb9b3750d49a95ae610fd75f1
akashdeep023 <ad3500476@gmail.com>
2024-09-12 18:26:42 +0530

    Create Stack using ArrayList & LinkedList

---
+++ 22-Stacks/StackC.java
@@ -0,0 +1,90 @@
+import java.util.*; // Import all package
+
+public class StackC {
+    // ArrayList --------------------------------
+    // static class Stack {
+    //     static ArrayList<Integer> list = new ArrayList<>();
+
+    //     public boolean isEmpty() { // Check Stack is empty
+    //         return list.size() == 0;
+    //     }
+
+    //     public void push(int data) { // Push data (add data to stack)
+    //         list.add(data);
+    //     }
+
+    //     public int pop() {
+    //         if (isEmpty()) { // Check stack is empty
+    //             return -1;
+    //         }
+    //         int data = list.get(list.size() - 1); // Pop data (remove from stack)
+    //         list.remove(list.size() - 1);
+    //         return data;
+    //     }
+
+    //     public int peek() { // Peek data (get from stack)
+    //         if (isEmpty()) { // Check stack is empty
+    //             return -1;
+    //         }
+    //         return list.get(list.size() - 1);
+    //     }
+    // }
+
+    // LinkedList -------------------------------- 
+    static class Node { // LinkedList Node class
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        static Node head = null;
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void push(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        // ArrayList & LinkedList --------------------------------
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
