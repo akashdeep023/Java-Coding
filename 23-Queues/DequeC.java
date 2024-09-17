1e734090d9494b4ca2452a407f1c5b8a7bdb0662
akashdeep023 <ad3500476@gmail.com>
2024-09-18 04:19:21 +0530

    Implement Stack & Queue using Deque

--- 23-Queues/DequeC.java
+++ 23-Queues/DequeC.java
@@ -2,6 +2,48 @@ import java.util.*;
 import java.util.LinkedList;
 
 public class DequeC {
+    // Implement Stack using Deque --------------------------------
+    static class Stack {
+        Deque<Integer> dq = new LinkedList<>();
+
+        public boolean isEmpty() {
+            return dq.isEmpty();
+        }
+
+        public void push(int data) {
+            dq.addLast(data);
+        }
+
+        public int pop() {
+            return dq.removeLast();
+        }
+
+        public int peek() {
+            return dq.getLast();
+        }
+    }
+
+    // Implement Queue using Deque --------------------------------
+    static class Queue {
+        Deque<Integer> dq = new LinkedList<>();
+
+        public boolean isEmpty() {
+            return dq.isEmpty();
+        }
+
+        public void add(int data) {
+            dq.addLast(data);
+        }
+
+        public int remove() {
+            return dq.removeFirst();
+        }
+
+        public int peek() {
+            return dq.getFirst();
+        }
+    }
+
     public static void main(String[] args) {
         // Deque (Double ended Queue) --------------------------------
         // Deque -> (Data structure) 
@@ -19,6 +61,28 @@ public class DequeC {
         System.out.println(deque); // 1 3 
         System.out.println("First element : " + deque.getFirst());
         System.out.println("Last element : " + deque.getLast());
+        System.out.println();
+
+        // Implement Stack using Deque --------------------------------
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println("peek = " + s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println();
+
+        // Implement Queue using Deque --------------------------------
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println("peek = " + q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
 
     }
 }
