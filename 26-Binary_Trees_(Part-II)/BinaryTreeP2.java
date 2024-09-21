e80d66d5e84ab86e42dd6b95903897f84e2f0453
akashdeep023 <ad3500476@gmail.com>
2024-09-22 01:39:59 +0530

    Diameter of Tree Approach-2

--- 26-Binary_Trees_(Part-II)/BinaryTreeP2.java
+++ 26-Binary_Trees_(Part-II)/BinaryTreeP2.java
@@ -26,17 +26,40 @@ public class BinaryTreeP2 {
     }
 
     // Diameter of Tree Approach-1 -------------------------------- TC -> O(n^2)
-    public static int diameter(Node root) {
+    public static int diameterA1(Node root) {
         if (root == null) {
             return 0;
         }
-        int leftDiam = diameter(root.left);
-        int rightDiam = diameter(root.right);
+        int leftDiam = diameterA1(root.left);
+        int rightDiam = diameterA1(root.right);
         int leftHt = height(root.left);
         int rightHt = height(root.left);
         int selfDiam = leftHt + rightHt + 1;
         return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
 
+    }
+
+    // Info class to store diameter and height of a tree --- helper class
+    static class Info {
+        int diam; // diameter
+        int ht; // height
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    // Diameter of Tree Approach-2 -------------------------------- TC -> O(n)
+    public static Info diameterA2(Node root) {
+        if (root == null) {
+            return new Info(0, 0); // return Info object
+        }
+        Info leftInfo = diameterA2(root.left);
+        Info rightInfo = diameterA2(root.right);
+        int finalDiam = Math.max((leftInfo.ht + rightInfo.ht + 1), Math.max(leftInfo.diam, rightInfo.diam));
+        int finalHt = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+        return new Info(finalDiam, finalHt);
     }
 
     public static void main(String[] args) {
@@ -66,7 +89,14 @@ public class BinaryTreeP2 {
          * 4 5 6
          */
         // Approach - 1 ---
-        System.out.println("Diameter A1 : " + diameter(root));
+        System.out.println("Diameter A1 : " + diameterA1(root)); // 5
+        System.out.println("Height A1 : " + height(root)); // 3
+        System.out.println();
+
+        // Approach - 2 ---
+        Info info = diameterA2(root);
+        System.out.println("Diameter A21 : " + info.diam); // 5
+        System.out.println("Height A21 : " + info.ht); // 3
 
     }
 }
