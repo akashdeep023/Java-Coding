4e67ff28fc5bc9c809a022636cd0876afcda8a98
akashdeep023 <ad3500476@gmail.com>
2024-09-22 18:19:33 +0530

    Lowest Common Ancestor

--- 27-Binary_Trees_(Part-III)/BinaryTreeP3.java
+++ 27-Binary_Trees_(Part-III)/BinaryTreeP3.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BinaryTreeP3 {
     // Create Node class
     static class Node {
@@ -25,6 +27,41 @@ public class BinaryTreeP3 {
         kLevelNode(root.right, level + 1, k);
     }
 
+    // Lowest Common Ancestor -------------------------------- TC -> O(n) SC -> O(n)
+    public static Node lowestCommonAncestor(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>(); // create array list to store path root to n1
+        getPath(root, n1, path1);
+        ArrayList<Node> path2 = new ArrayList<>(); // create array list to store path root to n2
+        getPath(root, n2, path2);
+
+        // last common node
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+        Node lca = path1.get(i - 1);
+        return lca;
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) { // TC -> O(n)
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.data == n) { // root is n
+            return true;
+        }
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+        if (foundLeft || foundRight) { // left or right side exists
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+
     public static void main(String[] args) {
         /* Create a Tree --------------------------------
                  1      
@@ -44,6 +81,13 @@ public class BinaryTreeP3 {
         // Kth level of a Tree --------------------------------
         System.out.print("Kth leverl of node : ");
         kLevelNode(root, 1, 3); // k = 3; Output -> 4,5,6,7
+        System.out.println("\n");
+
+        // Lowest Common Ancestor --------------------------------
+        int n1 = 4, n2 = 6; // Output -> 1
+        int n3 = 4, n4 = 5; // Output -> 2
+        System.out.println("LCA : " + lowestCommonAncestor(root, n1, n2).data);
+        System.out.println("LCA : " + lowestCommonAncestor(root, n3, n4).data);
 
     }
 }
