3d952c03897a9d60c04fd8abb47d37141b16d817
akashdeep023 <ad3500476@gmail.com>
2024-09-23 03:25:50 +0530

    Kth Ancestor of node

--- 27-Binary_Trees_(Part-III)/BinaryTreeP3.java
+++ 27-Binary_Trees_(Part-III)/BinaryTreeP3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class BinaryTreeP3 {
     // Create Node class
@@ -84,7 +85,6 @@ public class BinaryTreeP3 {
         int dist1 = lcaDist(lca, n1);
         int dist2 = lcaDist(lca, n2);
         return dist1 + dist2;
-
     }
 
     public static int lcaDist(Node lca, int n) {
@@ -103,6 +103,26 @@ public class BinaryTreeP3 {
         } else {
             return leftDist + 1;
         }
+    }
+
+    // Kth Ancestor of node -------------------------------- TC -> O(n)
+    public static int kthAncestor(Node root, int n, int k) { // return root to n distance
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = kthAncestor(root.left, n, k);
+        int rightDist = kthAncestor(root.right, n, k);
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+        int max = Math.max(leftDist, rightDist);
+        if (max + 1 == k) {
+            System.out.println(k + "th Ancestor of node " + n + " : " + root.data);
+        }
+        return max + 1;
     }
 
     public static void main(String[] args) {
@@ -144,6 +164,14 @@ public class BinaryTreeP3 {
         // n3 = 4 or n4 = 5 Output -> 2
         System.out.println("Minimum Distance between nodes : " + minDist(root, n1, n2));
         System.out.println("Minimum Distance between nodes : " + minDist(root, n3, n4));
+        System.out.println();
+
+        // Kth Ancestor of node --------------------------------
+        int n = 5;
+        int k = 2;
+        int k2 = 1;
+        kthAncestor(root, n, k);
+        kthAncestor(root, n, k2);
 
     }
 }
