81b803b3f02d85410ee1aaf3896aa8f419403fe5
akashdeep023 <ad3500476@gmail.com>
2024-09-24 04:07:41 +0530

    Build a BST

--- 28-Binary_Search_Trees_(Part-I)/BinarySearchTreeP1.java
+++ 28-Binary_Search_Trees_(Part-I)/BinarySearchTreeP1.java
@@ -1,4 +1,41 @@
 public class BinarySearchTreeP1 {
+    // BST Node is Same Structure of BT Node 
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Build a BST --------------------------------
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) { // insert left subtree
+            root.left = insert(root.left, val);
+        } else { // insert right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    // inorder traversal ---
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
     public static void main(String[] args) {
         /* 
          * Introduction Binary Search Tree (BTS) -------------------------------- 
@@ -16,5 +53,13 @@ public class BinarySearchTreeP1 {
          * Special Property -> 'Inorder' Traversal of BST givess a Sorted Sequence.
          * -> 1,2,3,4,5,6 (Sorted Binary Search Tree)
          */
+
+        // Build a Binary Search Tree (BST) --------------------------------
+        int values[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
     }
 }
