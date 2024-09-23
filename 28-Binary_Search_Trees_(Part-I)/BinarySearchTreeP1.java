b3d0d67acf89f04ba7ad23050c4b396bc45c0115
akashdeep023 <ad3500476@gmail.com>
2024-09-24 03:24:40 +0530

    Introduction BTS

---
+++ 28-Binary_Search_Trees_(Part-I)/BinarySearchTreeP1.java
@@ -0,0 +1,20 @@
+public class BinarySearchTreeP1 {
+    public static void main(String[] args) {
+        /* 
+         * Introduction Binary Search Tree (BTS) -------------------------------- 
+         * -> Use Binary Tree Properties (At max 2 children)  
+         * -> Left Subtree Nodes < Root 
+         * -> Right Subtree Nodes > Root
+         * -> Left & Right Subtree are also BST with no duplicates.
+         * 
+         *            4
+         *           / \       Root = 4
+         *          2   5      2(LC) < 4(Root)
+         *         / \   \     5(RC) > 4(Root)
+         *        1   3   6
+         * 
+         * Special Property -> 'Inorder' Traversal of BST givess a Sorted Sequence.
+         * -> 1,2,3,4,5,6 (Sorted Binary Search Tree)
+         */
+    }
+}
