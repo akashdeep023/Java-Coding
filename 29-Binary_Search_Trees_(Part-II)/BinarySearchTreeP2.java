9186f59ffffd938cd8c8ab8dbc797c4c69d5a4eb
akashdeep023 <ad3500476@gmail.com>
2024-09-27 02:21:48 +0530

    Convert BST to Balanced BST

--- 29-Binary_Search_Trees_(Part-II)/BinarySearchTreeP2.java
+++ 29-Binary_Search_Trees_(Part-II)/BinarySearchTreeP2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BinarySearchTreeP2 {
     // BST Node is Same Structure of BT Node 
     static class Node {
@@ -12,7 +14,7 @@ public class BinarySearchTreeP2 {
         }
     }
 
-    // Sorted array to Balanced BST --------------------------------
+    // Sorted array to Balanced BST -------------------------------- TC -> O(n)
     public static Node balancedBST(int[] arr, int start, int end) {
         if (start > end) {
             return null;
@@ -33,6 +35,36 @@ public class BinarySearchTreeP2 {
         preorder(root.right);
     }
 
+    // Convert BST to Balanced BST -------------------------------- TC -> O(n)
+    public static Node convertBalanceBST(Node root) {
+        // BST -> Sorted inorder ArrayList - O(n)
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+        // Sorted inorder -> Balance BST - O(n)
+        root = balancedBST2(inorder, 0, inorder.size() - 1);
+        return root;
+    }
+
+    public static void getInorder(Node root, ArrayList<Integer> inorder) { // Sorted inorder ArrayList TC -> O(n)
+        if (root == null) {
+            return;
+        }
+        getInorder(root.left, inorder);
+        inorder.add(root.data);
+        getInorder(root.right, inorder);
+    }
+
+    public static Node balancedBST2(ArrayList<Integer> inorder, int start, int end) { // Sorted inorder -> Balance BST (ArrayList) TC -> O(n)
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        Node root = new Node(inorder.get(mid));
+        root.left = balancedBST2(inorder, start, mid - 1);
+        root.right = balancedBST2(inorder, mid + 1, end);
+        return root;
+    }
+
     public static void main(String[] args) {
         // Sorted array to Balanced BST --------------------------------
         /*
@@ -45,6 +77,28 @@ public class BinarySearchTreeP2 {
         int arr[] = { 3, 5, 6, 8, 10, 11, 12 };
         Node root = balancedBST(arr, 0, arr.length - 1);
         preorder(root);
+        System.out.println("\n");
+
+        // Convert BST to Balanced BST --------------------------------
+        /*
+         *            8
+         *           / \                      8
+         *          6   10                  /   \
+         *         /      \       ->       5     11
+         *        5        11             / \    / \
+         *       /          \            3   6  10 12
+         *      3            12
+         */
+        Node root2 = new Node(8);
+        root2.left = new Node(6);
+        root2.left.left = new Node(5);
+        root2.left.left.left = new Node(3);
+        root2.right = new Node(10);
+        root2.right.right = new Node(11);
+        root2.right.right.right = new Node(12);
+        preorder(root2);
+        System.out.println();
+        preorder(convertBalanceBST(root2));
 
     }
 }
