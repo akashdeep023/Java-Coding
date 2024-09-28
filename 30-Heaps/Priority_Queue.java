20e210e7c712acf4c892788c56fc6d9f3da6c10f
akashdeep023 <ad3500476@gmail.com>
2024-09-29 01:10:43 +0530

    Priority Queue for Object

--- 30-Heaps/Priority_Queue.java
+++ 30-Heaps/Priority_Queue.java
@@ -2,6 +2,25 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Priority_Queue {
+    // Create Student Class (Student uses Comparable Interface of Student)
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override // Override campareTo function
+        public int compareTo(Student s2) { // compareTo function in Comparable<Student> Interface
+            return this.rank - s2.rank; // Rank base Sort
+            // this.rank - s2.rank = -ve => this < s2
+            // this.rank - s2.rank = +ve => this > s2
+            // this.rank - s2.rank = 0 => this == s2
+        }
+    }
+
     public static void main(String[] args) {
         // Priority Queue in JCF --------------------------------
         // Priority Queue is not a FIFO Data structure.
@@ -20,6 +39,20 @@ public class Priority_Queue {
         }
         // Accending Order - Output -> 1,2,3,4
         // Decending Order - Output -> 4,3,2,1
+        System.out.println();
 
+        // Priority Queue for Object --------------------------------
+        // PriorityQueue<Student> pqs = new PriorityQueue<>();
+        PriorityQueue<Student> pqs = new PriorityQueue<>(Comparator.reverseOrder());
+        pqs.add(new Student("A", 3)); // Add a new student ('A' -> Character & "A" -> String)
+        pqs.add(new Student("B", 5));
+        pqs.add(new Student("C", 2));
+        pqs.add(new Student("D", 8));
+        while (!pqs.isEmpty()) {
+            System.out.println(pqs.peek().name + "->" + pqs.peek().rank); // Peek - TC -> O(logn)
+            pqs.remove(); // Remove - TC -> O(1)
+        }
+        // Accending Order - Output -> C,A,B,D (Min Rank base sort)
+        // Decending Order - Output -> D,B,A,C (Max Rank base sort)
     }
 }
