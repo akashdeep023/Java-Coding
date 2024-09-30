a94a21f70bba27ea50e76a14c58e613c1419a7ca
akashdeep023 <ad3500476@gmail.com>
2024-10-01 02:52:33 +0530

    HashMap Introduction

---
+++ 31-Hashing/HashingC.java
@@ -0,0 +1,20 @@
+public class HashingC {
+    public static void main(String[] args) {
+        /* 
+         * Hashing --------------------------------
+         * Integer (123) ----HashFunction----> 48
+         * String (Akash) ----HashFunction----> 713
+         * 
+         * Hashing (Use Two data structures to generate)
+         *  ├── map (3 Types)
+         *  │ ├── HashMap (Important)
+         *  │ ├── LinkedHashMap
+         *  │ └── TreeMap
+         *  └── set (3 Types)
+         *    ├── HashSet (Important)
+         *    ├── LinkedHashSet
+         *    └── TreeSet 
+         */
+
+    }
+}
