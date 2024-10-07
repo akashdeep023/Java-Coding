32882fe2c44cd57c514c6faa2aeab55be3c03766
akashdeep023 <ad3500476@gmail.com>
2024-10-08 04:06:41 +0530

    Union & Intersection of 2 Arrays

--- 31-Hashing/HashingQ2.java
+++ 31-Hashing/HashingQ2.java
@@ -9,5 +9,39 @@ public class HashingQ2 {
             hs.add(num[i]);
         }
         System.out.println("Counts: " + hs.size()); // Output -> 7
+        System.out.println();
+
+        // Union & Intersection of 2 Arrays --------------------------------
+        int arr1[] = { 7, 3, 9 };
+        int arr2[] = { 6, 3, 9, 2, 9, 4 };
+        HashSet<Integer> set = new HashSet<>();
+
+        // union
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+        System.out.println("Union count : " + set.size());
+        System.out.println(set);
+        System.out.println();
+
+        // intersection
+        set.clear(); // Set Map use previous hashset
+        int count = 0;
+        HashSet<Integer> intersection = new HashSet<Integer>(); // Answer
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            if (set.contains(arr2[i])) {
+                count++;
+                set.remove(arr2[i]);
+                intersection.add(arr2[i]);
+            }
+        }
+        System.out.println("Intersection count : " + count);
+        System.out.println(intersection);
     }
 }
