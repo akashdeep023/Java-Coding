652ecfc0880fcfe891ad497fabb7337f9d5c4b88
akashdeep023 <ad3500476@gmail.com>
2024-10-11 03:30:06 +0530

    Graphs Introduction & Type of Graphs

---
+++ 33-Graphs_(Part-I)/GraphsP1.java
@@ -0,0 +1,33 @@
+public class GraphsP1 {
+    public static void main(String[] args) {
+        /* 
+         * Introduction --------------------------------
+         * Graphs -> Network of Vertices (Nodes)
+         *    Ex: 
+         *          1   4 —— 9
+         *           \ / \
+         *            6   3 —— 8
+         *                 \
+         *                  12
+         * 
+         *  Single node -> Vertex
+         *  Connection to node -> Edge
+         * 
+         * Type Of Graphs (Based on Edge) --------------------------------
+         * Direction ---
+         * 1. Uni-Directional (Directed Graph)
+         *    Ex: A ⇾ B
+         * 2. Un-Directed / Bi-Directional (Undirected Graph)
+         *    Ex: A — B 
+         *        A ←→ B
+         * 
+         * Weight ---
+         * 1. Weighted
+         *    Ex: Weight of Edge = +ve & -ve
+         * 
+         * 2. Un-Weighted
+         *    Ex: Weight of Edge = None
+         */
+
+    }
+}
