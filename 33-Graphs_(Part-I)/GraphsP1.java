e4025660ae3eeeb1dcc54fcf4abff0fa36f9c5d7
akashdeep023 <ad3500476@gmail.com>
2024-10-11 04:02:40 +0530

    Create a Graph (Using Adjacency List)

--- 33-Graphs_(Part-I)/GraphsP1.java
+++ 33-Graphs_(Part-I)/GraphsP1.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
 public class GraphsP1 {
+    // Create a Graph (Using Adjacency List) --------------------------------
+    static class Edge { // Create Edge for Graphs
+        int src; // Source
+        int dest; // Destination
+        int wt; // Weight
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
     public static void main(String[] args) {
         /* 
          * Introduction --------------------------------
@@ -38,7 +53,47 @@ public class GraphsP1 {
          * Ex: Google Maps, Social Network, Delivery Network (Shortest Cyclic Route), Physics & Chemistry (Atom-Molicules)
          *     Routing Algorithms, Machine Learning (Computation Graph), Dependency Graph (Windows->VS Code->Extensions)
          *     Computer Vision (Image Segmentation), Graph Databases (Nebula or neo4g), Research (Human Neurons), etc.
+         *
+         * Create a Graph (Using Adjacency List) --------------------------------
+         * 
+         *                (5)
+         *            0 —————— 1
+         *                    / \
+         *                (1)/   \(3)
+         *                  /     \
+         *                 2 ————— 3
+         *                 |  (1)
+         *              (2)|
+         *                 |
+         *                 4
          */
+        int V = 5; // Vertex count
+        @SuppressWarnings("unchecked") // Remove Warning
+        ArrayList<Edge>[] graph = new ArrayList[V]; // int[] arr = new int[V];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>(); // null -> empty arraylist initalizes
+        }
+        // 0-vertex
+        graph[0].add(new Edge(0, 1, 5));
+        // 1-vertex
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+        // 2-vertex
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+        // 3-vertex
+        graph[3].add(new Edge(3, 1, 3));
+        graph[3].add(new Edge(3, 2, 1));
+        // 4-vertex
+        graph[4].add(new Edge(4, 2, 2));
 
+        // 2's neighbors
+        System.out.print("2's neighbors : ");
+        for (int i = 0; i < graph[2].size(); i++) {
+            Edge e = graph[2].get(i);
+            System.out.print(e.dest + " ");
+        }
     }
 }
