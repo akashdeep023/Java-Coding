07e5ef7692df5723f4bb0b9f010d8c23415e39f4
akashdeep023 <ad3500476@gmail.com>
2024-10-17 03:46:58 +0530

    Connecting cities with minimum cost

---
+++ 37-Graphs_(Part-V)/GraphsP5_Connect_Cities.java
@@ -0,0 +1,51 @@
+import java.util.PriorityQueue;
+
+public class GraphsP5_Connect_Cities {
+    static class Edge implements Comparable<Edge> {
+        int dest;
+        int cost;
+
+        public Edge(int dest, int cost) {
+            this.dest = dest;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge e2) {
+            return this.cost - e2.cost; // ascending 
+        }
+    }
+
+    public static int connectCities(int cities[][]) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean visited[] = new boolean[cities.length];
+        pq.add(new Edge(0, 0));
+        int finalCost = 0;
+        while (!pq.isEmpty()) {
+            Edge curr = pq.remove();
+            if (!visited[curr.dest]) {
+                visited[curr.dest] = true;
+                finalCost += curr.cost;
+                for (int i = 0; i < cities[curr.dest].length; i++) {
+                    if (cities[curr.dest][i] != 0) {
+                        pq.add(new Edge(i, cities[curr.dest][i]));
+                    }
+                }
+            }
+        }
+        return finalCost;
+    }
+
+    public static void main(String[] args) {
+        // Connecting cities with minimum cost --------------------------------
+        // Find the minimum cost for connecting all cities on the map.
+        int cities[][] = {
+                { 0, 1, 2, 3, 4 },
+                { 1, 0, 5, 0, 7 },
+                { 2, 5, 0, 6, 0 },
+                { 3, 0, 6, 0, 0 },
+                { 4, 7, 0, 0, 0 }
+        };
+        System.out.println("Connecting cities cost : " + connectCities(cities)); // 10
+    }
+}
